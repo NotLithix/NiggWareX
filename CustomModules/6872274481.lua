aad85f2bb4acd082d76cef18145d76e37e827fe4
EasyWarePGN <trollclientv3@gmail.com>
2024-09-08 12:48:50 -0400

    Update 6872274481.lua

--- CustomModules/6872274481.lua
+++ CustomModules/6872274481.lua
@@ -1668,7 +1668,7 @@ do
 	end)
 	local textlabel = Instance.new("TextLabel")
 	textlabel.Size = UDim2.new(1, 0, 0, 36)
-	textlabel.Text = "The current version of vape is no longer being maintained, join the discord (click the discord icon) to get updates on the latest release."
+	textlabel.Text = "The current Version Of EasyWare May Be Updates Please Check The Server For Any Updates."
 	textlabel.BackgroundTransparency = 1
 	textlabel.ZIndex = 10
 	textlabel.TextStrokeTransparency = 0
